b41761fac5cb68eac905d62fbe4fb1b54041fec8
arter97 <qkrwngud825@gmail.com>
2021-01-23 18:34:18 +0100

    [DNM] sweet: init: workaround SafetyNet check
    * Override safetynet sensitive properties.
    * Also override fingerprint and security_patch properties.

--- init/init_grus.cpp
+++ init/init_grus.cpp
@@ -5,6 +5,8 @@
 
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/_system_properties.h>
 
 #include "vendor_init.h"
@@ -37,10 +39,64 @@ void property_override_multiple(std::string type, char const value[])
         property_override((char *)&(prop_list[i] + type)[0], value);
 }
 
+/* From Magisk@jni/magiskhide/hide_utils.c */
+static const char *snet_prop_key[] = {
+    "ro.boot.vbmeta.device_state",
+    "ro.boot.verifiedbootstate",
+    "ro.boot.flash.locked",
+    "ro.boot.selinux",
+    "ro.boot.veritymode",
+    "ro.boot.warranty_bit",
+    "ro.warranty_bit",
+    "ro.debuggable",
+    "ro.secure",
+    "ro.build.type",
+    "ro.build.tags",
+    "ro.build.selinux",
+    NULL
+};
+
+static const char *snet_prop_value[] = {
+    "locked",
+    "green",
+    "1",
+    "enforcing",
+    "enforcing",
+    "0",
+    "0",
+    "0",
+    "1",
+    "user",
+    "release-keys",
+    "1",
+    NULL
+};
+
+static void workaround_snet_properties() {
+    // Hide all sensitive props
+    for (int i = 0; snet_prop_key[i]; ++i) {
+        property_override(snet_prop_key[i], snet_prop_value[i]);
+    }
+}
+
 void vendor_load_properties()
 {
     property_override_multiple("name", "grus");
     property_override_multiple("model", "Mi 9 SE");
     property_override("ro.build.description",
         "grus-user 11 RKQ1.200826.002 V12.5.1.0.RFBMIXM release-keys");
+    property_override("ro.build.fingerprint",
+        "Xiaomi/grus/grus:11/RKQ1.200826.002/V12.5.1.0.RFBMIXM:user/release-keys");
+    property_override("ro.build.version.security_patch", "2021-04-01");
+
+    /* for HD on Amazon Prime Video
+    property_override_multiple("model", "Mi 10");
+    property_override("ro.build.description",
+        "umi-user 11 RKQ1.200826.002 V12.5.1.0.RJBMIXM release-keys");
+    property_override("ro.build.fingerprint",
+        "Xiaomi/umi_global/umi:11/RKQ1.200826.002/V12.5.1.0.RJBMIXM:user/release-keys");
+    property_override("ro.build.version.security_patch", "2021-04-01");
+    */
+
+    workaround_snet_properties();
 }
