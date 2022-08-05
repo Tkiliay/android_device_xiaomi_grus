f167604c0991eb1a88dfca0f8a80edb127f2bee1
Alexander Winkowski <dereference23@outlook.com>
2022-08-05 11:48:32 +0000

    grus: Fix AAPT config
    According to AOSP docs [1] apps should use xxhdpi resources
    for 440 dpi density.
    
    [1] https://developer.android.com/reference/android/util/DisplayMetrics#DENSITY_440
    
    Change-Id: Ib591e11a93b9814fabf772136096bde69e202e77

--- device.mk
+++ device.mk
@@ -12,7 +12,7 @@ $(call inherit-product, device/xiaomi/sdm710-common/sdm710.mk)
 
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Audio configs
 PRODUCT_COPY_FILES += \
