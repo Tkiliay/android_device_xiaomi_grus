83d4397f1c222854d665660d9c0f0d972b6c0e86
Tkiliay <sackclouths@outlook.com>
2022-10-23 09:51:45 +0800

    grus: Delete overlay-lineage

--- device.mk
+++ device.mk
@@ -24,8 +24,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # QDCM
 PRODUCT_COPY_FILES += \
