18a920d1f23bb2ebe136bd433fd54705a5d50e5b
Tkiliay <sackclouths@outlook.com>
2022-11-20 12:08:25 +0800

    grus: CherishOS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_grus.mk
+    $(LOCAL_DIR)/cherish_grus.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_grus-user \
-    lineage_grus-userdebug \
-    lineage_grus-eng
+    cherish_grus-user \
+    cherish_grus-userdebug \
+    cherish_grus-eng
