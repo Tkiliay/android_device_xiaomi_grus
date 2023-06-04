860e8f2904455d4b8fa200e14032e6cbade28393
Tkiliay <sackclouths@outlook.com>
2023-06-04 08:06:38 +0800

    grus: cherish-ify

--- cherish_grus.mk
+++ cherish_grus.mk
@@ -8,13 +8,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common cherish stuff.
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Inherit from grus device
 $(call inherit-product, device/xiaomi/grus/device.mk)
 
-PRODUCT_NAME := lineage_grus
+PRODUCT_NAME := cherish_grus
 PRODUCT_DEVICE := grus
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
@@ -26,3 +26,15 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="grus-user 11 RKQ1.200826.002 V12.5.1.0.RFBMIXM release-keys"
 
 BUILD_FINGERPRINT := Xiaomi/grus/grus:11/RKQ1.200826.002/V12.5.1.0.RFBMIXM:user/release-keys
+
+# FOD animations
+EXTRA_UDFPS_ANIMATIONS := true
+
+## Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Inherit some props from Cherish
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=Toyohama
+
+CHERISH_BUILD_TYPE=OFFICIAL
