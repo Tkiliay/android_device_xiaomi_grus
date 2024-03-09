a2c6c927612c8691a3c7d7825812a1e03db1d38e
Tkiliay <sackclouths@outlook.com>
2024-03-09 13:31:42 +0800

    grus: Update blobs from V12.5.4.0.RFBCNXM
    Change-Id: I7643dccae1daac3de68ff201651b64f325dd4d23

--- aosp_grus.mk
+++ aosp_grus.mk
@@ -23,5 +23,5 @@ PRODUCT_MODEL := MI 9 SE
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="grus-user 11 RKQ1.200826.002 V12.5.1.0.RFBMIXM release-keys" \
-    BuildFingerprint=Xiaomi/grus/grus:11/RKQ1.200826.002/V12.5.1.0.RFBMIXM:user/release-keys
+    BuildDesc="grus-user 11 RKQ1.200826.002 V12.5.4.0.RFBCNXM release-keys" \
+    BuildFingerprint=Xiaomi/grus/grus:11/RKQ1.200826.002/V12.5.4.0.RFBCNXM:user/release-keys
