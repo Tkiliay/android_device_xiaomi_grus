3a58ebdfe1bf6f82f9a7d5ce4e2f0ee7e987d42a
Tkiliay <sackclouths@gmail.com>
2024-07-26 23:16:21 +0800

    grus: Patch com.qti.chi.override.so The value 0x7D0 at 000863B0 only works in the miui camera buffer and causes interface lag in gcam, remove that buffer.
    before: MOV.W R2, #0x7D0 ; unsigned int
    
    after: MOV.W R2, #0 ; unsigned int
    
    Change-Id: I1f1da8d1f68d39126f7c026128831bc54e703615

--- extract-files.py
+++ extract-files.py
@@ -37,6 +37,8 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib/hw/audio.primary.grus.so': blob_fixup()
         .binary_regex_replace(b'/vendor/lib/liba2dpoffload\\.so', b'liba2dpoffload_grus.so\x00\x00\x00\x00\x00\x00\x00')
         .binary_regex_replace(b'/vendor/lib/libssrec\\.so', b'libssrec_grus.so\x00\x00\x00\x00\x00\x00\x00'),
+    'vendor/lib/hw/com.qti.chi.override.so': blob_fixup()
+        .sig_replace('4F F4 FA 62 11', '4F F0 00 02 11'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
